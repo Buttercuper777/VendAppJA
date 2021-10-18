0c6f765c5247480f83d46a935887dc90c3a923bf
buttercuper777 <webmast911@gmail.com>
2021-10-18 19:38:24 +0300

    Fix admin routing again

--- src/Components/AdminArea/Admin.jsx
+++ src/Components/AdminArea/Admin.jsx
@@ -3,30 +3,29 @@ import s from './Admin.module.css';
 import Header from './Header/Header';
 import ItemsContent from './ItemsContent/ItemsContent';
 import CoinContent from './CoinsContent/CoinContent';
-import ReactDOM from "react-dom";
 import { BrowserRouter , Route, Switch } from "react-router-dom";
 
 export default class Admin extends Component{
-  constructor(props){
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.state = {
-      path : ''
-    }
-  }
-
-  handleChange() {
-    this.setState({path: "sasas"});
-  }
-
   render(){
     return(
+      <div>
       <BrowserRouter>
-          <Header/>
-          <Route path='/admin/'></Route>
-          <Route path='/admin/Coins'><CoinContent /></Route>
-          <Route path='/admin/Products'><ItemsContent /></Route>
+        <Header/>
+        <Route path='/admin/'></Route>
+        <Route path='/admin/Coins'><CoinContent /></Route>
+        <Route path='/admin/Products'><ItemsContent /></Route>
       </BrowserRouter>
+      sdcsdcsdc<br/>
+      sdcsdcsdc<br/>
+      sdcsdcsdc<br/>
+      sdcsdcsdc<br/>
+      sdcsdcsdc<br/>
+      sdcsdcsdc<br/>
+
+      sdcsdcsdc<br/>
+      sdcsdcsdc<br/>
+
+      </div>
     );
   }
 }
--- src/Components/AdminArea/Header/Header.jsx
+++ src/Components/AdminArea/Header/Header.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import s from '../Admin.module.css';
-import Admin from '../Admin';
-import { BrowserRouter, Link, NavLink } from 'react-router-dom';
+import { BrowserRouter, Link, NavLink, Route} from 'react-router-dom';
 
 export default class Header extends Component{
 
 render(){
+  const isActive = (path, match, location) => !!(match || path === location.pathname);
   return(
       <div className={s.Header}>
         <section>
           <div className={s.HeaderNav}>
-            <NavLink className={s.NavLink} to="./Products" activeStyle={{color: '#6c80f0'}}>Продукты</NavLink>
-            <NavLink className={s.NavLink} to="./Coins" activeStyle={{color: '#6c80f0'}}>Монеты</NavLink>
+            <NavLink className={s.NavLink} to="/Admin/Products" activeStyle={{color: '#6c80f0'}} isActive={isActive.bind(this, '/Admin/Products')}>Продукты</NavLink>
+            <NavLink className={s.NavLink} to="/Admin/Coins" activeStyle={{color: '#6c80f0'}} isActive={isActive.bind(this, '/Admin/Coins')}>Монеты</NavLink>
           </div>
         </section>
       </div>
